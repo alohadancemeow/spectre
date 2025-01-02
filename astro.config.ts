edf84217a8ecbb462447878451cfbafeb3a84513
Alohadancemeow <nawongsak@gmail.com>
2025-01-02 18:23:36 +0700

    Add Tailwind CSS configuration and update site branding

--- astro.config.ts
+++ astro.config.ts
@@ -1,49 +1,52 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import expressiveCode from 'astro-expressive-code';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import spectre from './package/src';
+import expressiveCode from "astro-expressive-code";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import spectre from "./package/src";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://spectre.louisescher.dev',
-  output: 'static',
+  site: "https://spectre.louisescher.dev",
+  output: "static",
   integrations: [
     expressiveCode(),
     mdx(),
     sitemap(),
     spectre({
-      name: 'Spectre',
+      name: "Spectre dev",
       openGraph: {
         home: {
-          title: 'Spectre',
-          description: 'A minimalistic theme for Astro.'
+          title: "Spectre dev",
+          description: "A minimalistic theme for Astro.",
         },
         blog: {
-          title: 'Blog',
-          description: 'News and guides for Spectre.'
+          title: "Blog",
+          description: "News and guides for Spectre.",
         },
         projects: {
-          title: 'Projects'
-        }
+          title: "Projects",
+        },
       },
       giscus: {
-        repository: 'louisescher/spectre',
-        repositoryId: 'R_kgDONjm3ig',
-        category: 'General',
-        categoryId: 'DIC_kwDONjm3is4ClmBF',
-        mapping: 'pathname',
+        repository: "louisescher/spectre",
+        repositoryId: "R_kgDONjm3ig",
+        category: "General",
+        categoryId: "DIC_kwDONjm3is4ClmBF",
+        mapping: "pathname",
         strict: true,
         reactionsEnabled: true,
         emitMetadata: false,
-        lang: 'en',
-      }
-    })
+        lang: "en",
+      },
+    }),
+    tailwind(),
   ],
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
